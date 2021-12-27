ba185ff3942ed6483a960741162edd24b7dc3dcc
Tudorache Mihai-Ovidiu <ovi99or@gmail.com>
2021-12-27 17:41:01 +0200

    Added register and login functionality

--- Cinema_Booking_App/app/src/main/java/com/upt/cti/cinemabookingapp/Login.java
+++ Cinema_Booking_App/app/src/main/java/com/upt/cti/cinemabookingapp/Login.java
@@ -1,14 +1,78 @@
 package com.upt.cti.cinemabookingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
+    EditText mEmail, mPassword;
+    Button mLoginButton;
+    TextView mGoToRegister;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mEmail = findViewById(R.id.editTextEmail);
+        mPassword = findViewById(R.id.editTextPassword);
+        mLoginButton = findViewById(R.id.buttonLogIn);
+        mGoToRegister = findViewById(R.id.textNotRegistered);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    mEmail.setError("Email is required");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    mPassword.setError("Password is required");
+                    return;
+                }
+                if(password.length() < 6){
+                    mPassword.setError("Password must have more than 6 characters");
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                                }else{
+                                    Toast.makeText(Login.this,"Error! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+        mGoToRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Register.class));
+            }
+        });
     }
 }
--- Cinema_Booking_App/app/src/main/java/com/upt/cti/cinemabookingapp/MainActivity.java
+++ Cinema_Booking_App/app/src/main/java/com/upt/cti/cinemabookingapp/MainActivity.java
@@ -2,13 +2,25 @@ package com.upt.cti.cinemabookingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void logout(View view) {
+        mAuth.signOut();
+        startActivity(new Intent(getApplicationContext(), Login.class));
     }
 }
